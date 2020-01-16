c92defe45a4d8451bb79063f12c88735e4cbf6de
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2020-01-16 15:31:20 +0300

    Update workPage.js

--- Pages/workPage.js
+++ Pages/workPage.js
@@ -1,7 +1,7 @@
 const Page = require('./page');
 
 class WorkPage extends Page {
-    SearhField = element(by.css('[data-qa="search-input"]'));
+    SearchField = element(by.css('[data-qa="search-input"]'));
     SubmitButton = element(by.css('[class="supernova-search-submit-text"]'));
     WorkElements = element.all(by.xpath('//div[@data-qa="vacancy-serp__vacancy" or @data-qa="vacancy-serp__vacancy vacancy-serp__vacancy_premium"]'));
 }
